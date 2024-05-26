c5b7ffaef4e0d63cbdd86409ab5586c80faaea43
azrirafli72 <m.rafli.azriansyah@gmail.com>
2024-05-26 17:46:49 +0700

    Perubahan pada Tugas 5

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    return {
+      slide: ref(1),
+      autoplay: ref(true)
+    }
+  }
+}
+</script>
+
 <template>
   <q-layout view="hHh lpr fFf">
 
@@ -17,6 +30,30 @@
         <q-route-tab to="/page3" label="Get Started" />
       </q-tabs>
     </q-header>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide :name="1" img-src="https://plus.unsplash.com/premium_photo-1700822900314-9196749b974e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <q-carousel-slide :name="2" img-src="https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <q-carousel-slide :name="3" img-src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <q-carousel-slide :name="4" img-src="https://images.unsplash.com/photo-1470309864661-68328b2cd0a5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    </q-carousel>
+  </div>
 
     <q-page-container>
       <router-view />
