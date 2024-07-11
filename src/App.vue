c9206331a30a00c11ae66439f2f8c4054de3c20f
azrirafli72 <m.rafli.azriansyah@gmail.com>
2024-07-11 19:18:11 +0700

    Perubahan styling bang, lupa ngubah isi card dan footer

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ export default {
     return {
       slide: ref(1),
       autoplay: ref(true),
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      lorem: 'Lorem ipsum dolor sit amet'
     }
   }
 }
